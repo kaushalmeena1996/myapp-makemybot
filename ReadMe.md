c86ccc29662bb494bd323b63a7dd77a10b69575d
kaushal.meena1996 <kaushal.meena1996@gmail.com>
2017-01-20 21:24:32 +0530

    second commit

--- ReadMe.md
+++ ReadMe.md
@@ -2,47 +2,11 @@
 
 ## [ Synopsis ]
 
-A web application that provides a list of gameitems within a variety of categories and integrates third-party user registration and authentication.
-
-## [ Prerequisities ]
-
-- Python
-  - https://www.python.org/downloads/
-- Python Libraries
-  - Flask==0.9
-    - http://flask.pocoo.org/docs/0.11/installation/
-  - oauth2client==3.0.0
-    - https://oauth2client.readthedocs.io/en/latest/
-  - httplib2==0.9.2
-    - https://pypi.python.org/pypi/httplib2
-  - SQLAlchemy==1.0.15
-    - http://www.sqlalchemy.org/download.html
-  - requests==2.11.1
-    - http://docs.python-requests.org/en/master/user/install/
+A web application that lets user to create and teach their own custom chatbots.
 
 ## [ Usage ]
-As the app uses Google for authentication as the next step you have to obtain a client id and client secret from Google and Facebook:
 
-For Google
-
-- Go to the Google Developer Console.
-- Create a new project.
-- Go to APIs & auth - Consent screen and select a valid Email address.
-- Go to APIs & auth - Credentials and create a new Client ID.
-- Enter http://localhost:8000/oauth2callback/ in the Authorized redirect URIs field.
-- Click DownloadJSON icon and save it as 'g_client_secrets.json' in secret folder.
-
-For Facebook
-
-- Go to Facebook Developers and login with your account. Select Add a New App from the dropdown in the upper right: When asked to select a platform, click Basic Setup
-- Provide a Display Name and Contact Email. Select a Category and click Create App ID.
-- On the Product Setup page that follows, click Get Started next to Facebook Login: Scroll down to the Client OAuth Settings section and enter the following URL in the Valid OAuth redirect URIs field: http://localhost:8000/oauth2callback Click on Save Changes.
-- Select Settings in the left nav. Click Show to reveal the App Secret. (You may be required to re-enter your Facebook password.)
-- Save a copy of the App ID and App Secret in 'app-id' and 'app-secret' fields of 'fb_client_secrets.json' in secret folder.
-- On the same Basic Settings page, scroll down and click Add Platform: Select Website in the pop-up: In the Site URL field, enter the following: http://localhost:8000
-- Click Yes on the App Review page of your app to make it available to the public.
-
-Next you have to create the game-catalog.db To do so run in database folder
+Next you have to create the makemybot.db To do so run in database folder
 ~~~~
 python database_setup.py
 ~~~~
@@ -55,5 +19,5 @@ python dummyrecords.py
 
 To start the app simply run
 ~~~~
-python server.py
+python main.py
 ~~~~
--- main.py
+++ main.py
@@ -118,17 +118,6 @@ def cookie_check(f):
 def showLogin():
     """"Handler for LOGIN page which allows users to login."""
 
-    g_client_id = json.loads(
-        open('secret/g_client_secrets.json', 'r').read())['web']['client_id']
-    fb_app_id = json.loads(
-        open('secret/fb_client_secrets.json', 'r').read())['web']['app_id']
-    state = ''.join(random.choice(
-                    string.ascii_uppercase + string.digits)for x in xrange(32))
-    
-    login_session['state'] = state
-    login_session['g_client_id'] = g_client_id
-    login_session['fb_app_id'] = fb_app_id
-
     if 'user_id' in login_session:
         flash("you have alredy logged in.")
         return redirect(url_for('showHome'))
@@ -447,176 +436,6 @@ def showSetting02():
         flash("you must be logged in first.")
         return redirect(url_for('showLogin'))
 
-
-@app.route('/fbconnect', methods=['POST'])
-def fbconnect():
-    """Handler for LOGIN via facebook."""
-
-    if request.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    access_token = request.data
-    print "access token received %s " % access_token
-    app_id = json.loads(
-        open('secret/fb_client_secrets.json', 'r').read())['web']['app_id']
-    app_secret = json.loads(
-        open('secret/fb_client_secrets.json', 'r').read())['web']['app_secret']
-    url = ('https://graph.facebook.com/oauth/access_token?'
-           'grant_type=fb_exchange_token&client_id=%s&client_secret=%s'
-           '&fb_exchange_token=%s' % (app_id, app_secret, access_token))
-
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    # Use token to get user info from API
-    userinfo_url = "https://graph.facebook.com/v2.4/me"
-    # strip expire tag from access token
-    token = result.split("&")[0]
-    url = 'https://graph.facebook.com/v2.4/me?%s&fields=name,id,email' % token
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    # print "url sent for API access:%s"% url
-    # print "API JSON result: %s" % result
-    data = json.loads(result)
-
-    login_session['provider'] = 'facebook'
-    login_session['name'] = data["name"]
-    login_session['email'] = data["email"]
-    login_session['facebook_id'] = data["id"]
-    # The token must be stored in the login_session in order to properly
-    # logout, let's strip out the information before the equals sign in our
-    # token
-    stored_token = token.split("=")[1]
-    login_session['access_token'] = stored_token
-    # Get user image_url
-    url = ('https://graph.facebook.com/v2.4/me/picture?%s&redirect=0&'
-           'height=200&width=200' % token)
-    h = httplib2.Http()
-    result = h.request(url, 'GET')[1]
-    data = json.loads(result)
-    login_session['image_url'] = data["data"]["url"]
-
-    # See if user exists
-    user_id = getUserID(login_session['email'])
-    if not user_id:
-        user_id = createUser(login_session)
-        flash("note: both your name and password are set to your email.")
-
-    login_session['user_id'] = user_id
-
-    flash("you are now logged in as %s." % login_session['name'])
-
-    response = make_response(json.dumps('login successful.'), 200)
-    cookie_val = make_secure_val(str(login_session['user_id']))
-    response.set_cookie('user_id', cookie_val)
-    return response
-
-
-@app.route('/fbdisconnect')
-def fbdisconnect():
-    """Handler for DISCONNECTING from facebook."""
-
-    facebook_id = login_session['facebook_id']
-    # The access token must me included to successfully logout
-    access_token = login_session['access_token']
-    url = 'https://graph.facebook.com/%s/permissions?access_token=%s' % (
-        facebook_id, access_token)
-    h = httplib2.Http()
-    result = h.request(url, 'DELETE')[1]
-    return "you have been logged out"
-
-
-@app.route('/gconnect', methods=['POST'])
-def gconnect():
-    """Handler for LOGIN via google."""
-
-    # Validate state token
-    if request.args.get('state') != login_session['state']:
-        response = make_response(json.dumps('Invalid state parameter.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-    # Obtain authorization code
-    code = request.data
-
-    try:
-        # Upgrade the authorization code into a credentials object
-        oauth_flow = flow_from_clientsecrets(
-            'secret/g_client_secrets.json', scope='')
-        oauth_flow.redirect_uri = 'postmessage'
-        credentials = oauth_flow.step2_exchange(code)
-    except FlowExchangeError:
-        response = make_response(
-            json.dumps('Failed to upgrade the authorization code.'), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    # Check that the access token is valid.
-    client_id = json.loads(
-        open('secret/g_client_secrets.json', 'r').read())['web']['client_id']
-    access_token = credentials.access_token
-    url = ('https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
-           % access_token)
-    h = httplib2.Http()
-    result = json.loads(h.request(url, 'GET')[1])
-    # If there was an error in the access token info, abort.
-    if result.get('error') is not None:
-        response = make_response(json.dumps(result.get('error')), 500)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    # Verify that the access token is used for the intended user.
-    gplus_id = credentials.id_token['sub']
-    if result['user_id'] != gplus_id:
-        response = make_response(
-            json.dumps("Token's user ID doesn't match given user ID."), 401)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    # Verify that the access token is valid for this app.
-    if result['issued_to'] != client_id:
-        response = make_response(
-            json.dumps("Token's client ID does not match app's."), 401)
-        print "Token's client ID does not match app's."
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    stored_credentials = login_session.get('credentials')
-    stored_gplus_id = login_session.get('gplus_id')
-    if stored_credentials is not None and gplus_id == stored_gplus_id:
-        response = make_response(json.dumps('Current user is already '
-                                            'connected.'), 200)
-        response.headers['Content-Type'] = 'application/json'
-        return response
-
-    # Store the access token in the session for later use.
-    login_session['credentials'] = credentials.access_token
-    login_session['gplus_id'] = gplus_id
-
-    # Get user info
-    userinfo_url = "https://www.googleapis.com/oauth2/v1/userinfo"
-    params = {'access_token': credentials.access_token, 'alt': 'json'}
-    answer = requests.get(userinfo_url, params=params)
-
-    data = answer.json()
-
-    login_session['provider'] = 'google'
-    login_session['name'] = data['name']
-    login_session['image_url'] = data['picture']
-    login_session['email'] = data['email']
-
-    # See if user exists
-    user_id = getUserID(login_session['email'])
-    if not user_id:
-        user_id = createUser(login_session)
-        flash("note: both your name and password are set to your email.")
-    login_session['user_id'] = user_id
-
-    flash("you are now logged in as %s." % login_session['name'])
-
-    response = make_response(json.dumps('login successful.'), 200)
-    cookie_val = make_secure_val(str(login_session['user_id']))
-    response.set_cookie('user_id', cookie_val)
-    return response
 
 # User Helper Functions
 
@@ -1541,27 +1360,13 @@ def showLogout():
     """DISCONNECTS the user based on provider."""
 
     if 'provider' in login_session:
-        if login_session['provider'] == 'google':
-            gdisconnect()
-            if 'gplus_id' in login_session:
-                del login_session['gplus_id']
-            if 'credentials' in login_session:
-                del login_session['credentials']
-        if login_session['provider'] == 'facebook':
-            fbdisconnect()
-            if 'facebook_id' in login_session:
-                del login_session['facebook_id']
-
+    	
         if 'user_id' in login_session:
             del login_session['name']
         if 'email' in login_session:
             del login_session['email']
-        if 'image_url' in login_session:
-            del login_session['image_url']
         if 'user_id' in login_session:
             del login_session['user_id']
-        if 'provider' in login_session:
-            del login_session['provider']
 
         flash("you have successfully been logged out.")
         response = make_response(redirect(url_for('showHome')))
