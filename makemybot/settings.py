2db7facb08b3cab725b8f0ead900c11789e65c6f
kaushal.meena1996 <kaushal.meena1996@gmail.com>
2017-10-04 20:50:35 +0530

    changed static root

--- makemybot/settings.py
+++ makemybot/settings.py
@@ -118,7 +118,7 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
