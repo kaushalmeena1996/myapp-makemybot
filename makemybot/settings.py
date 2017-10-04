d38fe3936b90c8e24269c41e30541c6ba4ed2c89
kaushal.meena1996 <kaushal.meena1996@gmail.com>
2017-10-04 20:47:21 +0530

    few config changes

--- makemybot/settings.py
+++ makemybot/settings.py
@@ -118,14 +118,9 @@ PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 STATIC_URL = '/static/'
 
 STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # Media files (Avtaars, Brains, Logs)
 MEDIA_URL = '/media/'
